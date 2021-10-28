47d2560d704cfadddf81070ff66aaecc866dc339
Bauauto <bauauto@gmail.com>
2021-10-28 21:42:38 +0700

    day code lan dau

---
+++ src/Table.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Component } from "react";
+
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = (props) => {
+  const rows = props.characterData.sections.map((row) => {
+    return (
+      <tr>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
+};
+
+class Table extends Component {
+  render() {
+    const characterData = this.props;
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} />
+      </table>
+    );
+  }
+}
+
+export default Table;
